2c1aad86997be9edf7731928889acbdd8001d8c5
Anderson Rafael Moura Cunha <anderson.rafael.cunha@gmail.com>
2025-03-22 15:16:12 -0300

    Client type impl

--- src/main/java/com/prj/agile/entity/Client.java
+++ src/main/java/com/prj/agile/entity/Client.java
@@ -4,13 +4,16 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @Setter
 @Entity(name = "Client")
 @Table(name = "client",
         schema= "onboarding",
         uniqueConstraints = {
-            @UniqueConstraint(name="client_document_unique", columnNames = "document")
+            @UniqueConstraint(name="client_document_unique", columnNames = "document"),
+            @UniqueConstraint(name="client_email_unique", columnNames = "email")
         }
 )
 public class Client {
@@ -31,9 +34,33 @@ public class Client {
     @Column(name="name", columnDefinition = "TEXT", nullable = false)
     private String name;
 
-    public Client(String document, String name){
+    @Column(name="birth_date", columnDefinition = "DATE", nullable = false)
+    private Date birthDate;
+
+    @Column(name="created_at", columnDefinition = "TIMESTAMP", nullable = false)
+    private Date createdAt;
+
+    @Column(name="pep", columnDefinition = "BOOLEAN", nullable = false)
+    private Boolean pep;
+
+    @Column(name = "email", nullable = false, columnDefinition = "TEXT")
+    private String email;
+
+    @Column(name = "status", nullable = false, columnDefinition = "VARCHAR")
+    private String status;
+
+    @OneToOne
+    @JoinColumn(name = "client_type_id")
+    private ClientType clientType;
+
+
+    public Client(String document, String name, Date birthDate, Date createdAt, Boolean pep, String email, String status) {
         this.document = document;
         this.name = name;
+        this.birthDate = birthDate;
+        this.createdAt = createdAt;
+        this.pep = pep;
+        this.email = email;
+        this.status = status;
     }
-
 }
